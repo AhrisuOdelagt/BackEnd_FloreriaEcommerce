3d17f72b415ac0478127a5adb860833c7d66dbee
Ahrisu <arisuodelagtv@gmail.com>
2023-05-27 20:54:27 -0600

    Instalación del paquete CORS

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import clienteRoutes from "./routes/clienteRoutes.js"
+import temporadaRoutes from "./routes/temporadaRoutes.js"
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 dotenv.config();
 connectDB();
 

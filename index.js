95234a9cf350ed95a3e40f99c0df0585d73dfed4
Guiillerm0 <dguilleing@gmail.com>
2023-05-30 23:51:23 -0600

    Controlador y Ruta Producto

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import clienteRoutes from "./routes/clienteRoutes.js"
+import productosRoutes from "./routes/productosRoutes.js"
 
 const app = express();
 app.use(express.json());
@@ -11,6 +12,7 @@ connectDB();
 // Enrutamiento
 app.use("/api/cliente", clienteRoutes);
 // app.use("/api/temporada", temporadaRoutes);
+app.use("/api/producto", productosRoutes);
 
 
 // Ocultamiento del puerto de conexión
